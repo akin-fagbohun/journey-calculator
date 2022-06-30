e2373a8dccf7f612883938bfbf30a22b00897038
Akin Fagbohun <a_fagbohun@yahoo.co.uk>
2022-06-30 09:50:16 +0100

    finally fixed cors

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const handleClick = () => {
+    fetch('https://7302htasp6.execute-api.eu-west-1.amazonaws.com/v1/airport').then((res) => {
+      res.json().then((data) => console.log(data));
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button type="button" onClick={handleClick}>
+          App
+        </button>
       </header>
     </div>
   );
